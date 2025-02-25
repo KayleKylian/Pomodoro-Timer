c8d1ccdb502ab2a6aa4b248c8aa257c8a7a668b1
Italo Santos <94938791+KayleKylian@users.noreply.github.com>
2025-02-25 11:26:28 -0300

    Adiciona suporte para exibir estado de pausa ou trabalho no temporizador

--- src/display_status.c
+++ src/display_status.c
@@ -14,12 +14,17 @@ void initial_display()  {
     ssd1306_send_data(&ssd);
 }
 
-void update_timer(int minutes, int seconds) {
+void update_timer(int minutes, int seconds, bool on_break) {
     char timer[16];
     snprintf(timer, sizeof(timer), "%02d:%02d", minutes, seconds);
 
     ssd1306_fill(&ssd, false);
     ssd1306_rect(&ssd, 0, 0, WIDTH, HEIGHT, true, false);
-    ssd1306_draw_string(&ssd, timer, 10, 10);
+    if (on_break) {
+        ssd1306_draw_string(&ssd, "Break", 10, 10);
+    } else {
+        ssd1306_draw_string(&ssd, "Work", 10, 10);
+    }
+    ssd1306_draw_string(&ssd, timer, 10, 30);
     ssd1306_send_data(&ssd);
 }
--- src/display_status.h
+++ src/display_status.h
@@ -1,7 +1,9 @@
 #ifndef DISPLAY_STATUS_H
 #define DISPLAY_STATUS_H
 
+#include <stdbool.h>
+
 void initial_display(void);
-void update_timer(int minutes, int seconds);
+void update_timer(int minutes, int seconds, bool on_break);
 
 #endif // DISPLAY_STATUS_H

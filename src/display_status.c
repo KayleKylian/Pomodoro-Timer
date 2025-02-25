8346ecc2f072cdf295e23d3e9c38f7f159bceb1d
Italo Santos <94938791+KayleKylian@users.noreply.github.com>
2025-02-25 10:06:32 -0300

    Adiciona funções para exibir e atualizar o temporizador no display SSD1306

---
+++ src/display_status.c
@@ -0,0 +1,25 @@
+#include "display_status.h"
+#include "../inc/ssd1306.h"
+#include <stdio.h>
+
+extern ssd1306_t ssd;
+
+void initial_display()  {
+    ssd1306_fill(&ssd, false);
+    
+    ssd1306_rect(&ssd, 0, 0, WIDTH, HEIGHT, true, false);
+    ssd1306_draw_string(&ssd, "Pomodoro Timer", 10, 10);
+    ssd1306_draw_string(&ssd, "A to start", 10, 30);
+    ssd1306_draw_string(&ssd, "B to pause", 10, 40);
+    ssd1306_send_data(&ssd);
+}
+
+void update_timer(int minutes, int seconds) {
+    char timer[16];
+    snprintf(timer, sizeof(timer), "%02d:%02d", minutes, seconds);
+
+    ssd1306_fill(&ssd, false);
+    ssd1306_rect(&ssd, 0, 0, WIDTH, HEIGHT, true, false);
+    ssd1306_draw_string(&ssd, timer, 10, 10);
+    ssd1306_send_data(&ssd);
+}
---
+++ src/display_status.h
@@ -0,0 +1,7 @@
+#ifndef DISPLAY_STATUS_H
+#define DISPLAY_STATUS_H
+
+void initial_display(void);
+void update_timer(int minutes, int seconds);
+
+#endif // DISPLAY_STATUS_H

e594d0b5f99f892b0a80c551c3b47ab828c8a344
Italo Santos <94938791+KayleKylian@users.noreply.github.com>
2025-02-25 11:26:34 -0300

    Adiciona suporte para ciclos de trabalho e pausa no temporizador Pomodoro

--- src/Pomodoro-Timer.c
+++ src/Pomodoro-Timer.c
@@ -10,10 +10,14 @@ void gpio_irq_handler(uint gpio, uint32_t events);
 bool timer_callback(repeating_timer_t *rt);
 
 // Variables
-int minutes = 25;
+int work_minutes = 25;
+int break_minutes = 5;
+int minutes = 1;
 int seconds = 0;
+bool on_break = false;
 bool timer_running = false;
 repeating_timer_t timer;
+extern ssd1306_t ssd;
 
 int main()
 {
@@ -30,12 +34,14 @@ int main()
     }
 }
 
-void gpio_irq_handler(uint gpio, uint32_t events) {
+void gpio_irq_handler(uint gpio, uint32_t events) 
+{
     static absolute_time_t last_interrupt_time = {0};
     absolute_time_t current_time = get_absolute_time();
     int64_t time_diff = absolute_time_diff_us(last_interrupt_time, current_time);
 
-    if (time_diff < 300000) { // 300ms debounce time
+    if (time_diff < 300000) 
+    { // 300ms debounce time
         return;
     }
 
@@ -46,33 +52,67 @@ void gpio_irq_handler(uint gpio, uint32_t events) {
             printf("Pomodoro already running\n");
             return;
         }
+
         add_repeating_timer_ms(-1000, timer_callback, NULL, &timer);
+        if (on_break) {
+            gpio_put(LED_RED, 0);
+            gpio_put(LED_BLUE, 1);
+            gpio_put(LED_GREEN, 0);
+        } else {
+            gpio_put(LED_RED, 0);
+            gpio_put(LED_BLUE, 0);
+            gpio_put(LED_GREEN, 1);
+        }
+        timer_running = true;
+        printf("Pomodoro started\n");
+        update_timer(minutes, seconds, on_break); // Update display immediately
         return;
     } else if (gpio == BUTTON_B) {
         if (timer_running) {
             printf("Pomodoro paused\n");
             timer_running = false;
+            gpio_put(LED_BLUE, 0);
+            gpio_put(LED_GREEN, 1);
+            gpio_put(LED_RED, 1);
             cancel_repeating_timer(&timer);
+            ssd1306_draw_string(&ssd, "Paused", 60, 10);
+            ssd1306_send_data(&ssd);
             return;
         }
     } else if (gpio == BUTTON_JS) {
         printf("Pomodoro finished\n");
-        minutes = 25;
+        minutes = work_minutes;
         seconds = 0;
         timer_running = false;
+        on_break = false;
+        gpio_put(LED_RED, 0);
+        gpio_put(LED_GREEN, 0);
         cancel_repeating_timer(&timer);
         initial_display();
         return;
     }
 }
 
-bool timer_callback(repeating_timer_t *rt) {
+bool timer_callback(repeating_timer_t *rt) 
+{
     if (seconds == 0) {
         if (minutes == 0) {
-            printf("Pomodoro finished\n");
-            init_display();
-            timer_running = false;
-            return false;
+            if (on_break) {
+                minutes = work_minutes;
+                seconds = 0;
+                on_break = false;
+                gpio_put(LED_GREEN, 1);
+                gpio_put(LED_BLUE, 0);
+                printf("Break finished\n");
+            } else {
+                minutes = break_minutes;
+                seconds = 0;
+                on_break = true;
+                gpio_put(LED_GREEN, 0);
+                gpio_put(LED_BLUE, 1);
+                printf("Work finished\n");
+            }
+            return true;
         }
         minutes--;
         seconds = 59;
@@ -80,7 +120,6 @@ bool timer_callback(repeating_timer_t *rt) {
         seconds--;
     }
     
-    timer_running = true;
-    update_timer(minutes, seconds);
+    update_timer(minutes, seconds, on_break);
     return true;
 }
